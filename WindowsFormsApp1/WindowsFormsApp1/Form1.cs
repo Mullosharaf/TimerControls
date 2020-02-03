f3813a6766adaed184c20e15bd1edb940da677ad
User <User@WIN-CNVI5BMCFJB>
2020-02-03 11:56:26 +0500

    MessageBox Dialog By Example

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,32 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+        "Выберите один из вариантов",
+        "Сообщение",
+        MessageBoxButtons.YesNo,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1,
+        MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+        "Окрасить кнопку в красный цвет?",
+        "Сообщение",
+        MessageBoxButtons.YesNo,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1,
+        MessageBoxOptions.DefaultDesktopOnly);
+
+            if (result == DialogResult.Yes)
+                button2.BackColor = Color.Red;
+
+            //this.TopMost = true;
+        }
     }
 }
